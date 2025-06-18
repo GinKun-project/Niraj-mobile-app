0dc6198db735f4814244ad9a33d7bb6fe2e5188c
GinKun-project <chresthaniraj101@gmail.com>
2025-06-18 09:26:18 +0545

    service locator

--- lib/app/service_locator/service_locator.dart
+++ lib/app/service_locator/service_locator.dart
@@ -1,16 +1,31 @@
 import 'package:get_it/get_it.dart';
+import 'package:shadow_clash_frontend/app/hive/hive_service.dart';
 import 'package:shadow_clash_frontend/features/auth/data/auth_remote_data_source.dart';
+import 'package:shadow_clash_frontend/features/auth/data/data_source/local_user_data_source.dart';
 import 'package:shadow_clash_frontend/features/auth/domain/auth_repository.dart';
 import 'package:shadow_clash_frontend/features/auth/presentation/view_model/login/login_view_model.dart';
 import 'package:shadow_clash_frontend/features/auth/presentation/view_model/signup/signup_view_model.dart';
+import 'package:shadow_clash_frontend/features/splash/presentation/view_model/splash_view_model.dart';
 
-final getIt = GetIt.instance;
+final GetIt getIt = GetIt.instance;
 
-Future<void> setupServiceLocator() async {
-  // Repositories
-  getIt.registerLazySingleton<AuthRepository>(() => AuthRemoteDataSource());
+Future<void> initServiceLocator() async {
+  // ✅ Core
+  await HiveService.initializeHive();
 
-  // ViewModels
+  // ✅ Data Sources
+  getIt.registerLazySingleton<AuthRemoteDataSource>(
+    () => AuthRemoteDataSource(),
+  );
+  getIt.registerLazySingleton<LocalUserDataSource>(() => LocalUserDataSource());
+
+  // ✅ Repository
+  getIt.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(remoteDataSource: getIt()),
+  );
+
+  // ✅ ViewModels
   getIt.registerFactory(() => LoginViewModel(getIt()));
   getIt.registerFactory(() => SignupViewModel(getIt()));
+  getIt.registerFactory(() => SplashViewModel());
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shadow_clash_frontend/app/app.dart';
-import 'package:shadow_clash_frontend/app/hive/hive_service.dart';
 import 'package:shadow_clash_frontend/app/service_locator/service_locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize dependency injection
-  await HiveService.initializeHive();
-  await setupServiceLocator();
-
+  await initServiceLocator(); // ✅ Use updated method
   runApp(const App());
 }
